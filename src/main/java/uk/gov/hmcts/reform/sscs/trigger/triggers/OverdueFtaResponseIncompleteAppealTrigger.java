2f5ff419635a5b01184053a41c0ae64c3421451b
Gemma Talbot <gemma.talbot@solirius.com>
2025-11-20 16:34:07 +0000

    add overdue Response

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/trigger/triggers/OverdueFtaResponseIncompleteAppealTrigger.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.reform.sscs.trigger.triggers;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+public class OverdueFtaResponseIncompleteAppealTrigger extends DateTrigger {
+
+    private static final String DATE_FIELD = "data.directionDueDate";
+
+    private static final String EVENT_NAME = "overdueResponse";
+
+    public OverdueFtaResponseIncompleteAppealTrigger(LocalDate triggerDate) {
+        super(triggerDate, DATE_FIELD, triggerDate, EVENT_NAME);
+    }
+}
